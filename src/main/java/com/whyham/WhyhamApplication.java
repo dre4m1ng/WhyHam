424563fb34780401121c103d9425816c57f8fa57
je0nh <winter0pear@gmail.com>
2024-07-26 19:30:28 +0900

    SpringBoot build

---
+++ src/main/java/com/whyham/WhyhamApplication.java
@@ -0,0 +1,13 @@
+package com.whyham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyhamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhyhamApplication.class, args);
+    }
+
+}
